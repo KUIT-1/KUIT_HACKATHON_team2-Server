3337b7a98446eb4912e8f21e8427323a6b45d02e
limsubinn <mymyx02@naver.com>
2023-06-24 05:32:59 +0900

    chore: @SpringBootTest 제거

--- src/test/java/com/example/starbucks/StarbucksApplicationTests.java
+++ src/test/java/com/example/starbucks/StarbucksApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.starbucks;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class StarbucksApplicationTests {
 
 	@Test
